4b6e25fd08c1c2eeb43174eabd38183103ea4892
gauravPanditr <gauravpandit7856@gmail.com>
2024-08-07 11:23:11 +0530

    Basic UberProject setup

---
+++ model/user.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const brcypt=require('brcypt');
+
+const userSchema= new mongoose.Schema({
+
+name:String,
+email:{
+    type:String,
+    unique:true,
+},
+password:String,
+
+role:{
+    type:String,
+    enum:['driver','passenger']
+},
+location:{
+  type:{
+    type:String,
+    enum:['Point'],
+    default:'Point'
+  },
+  coordinates:{
+    type:[Number],
+    default:[0,0]
+  }
+}
+
+
+});
+//this is a pre save middleware that run
+userSchema.pre('save',async function (password) {
+  return brcypt.hash(this.password)
+    if(!this.isModified('password'))
+       return next();
+      this.password=await brcypt.hash(this.password);
+      next();
+});
+
+userSchema.methods.comparePassword=async function (password) {
+   return brcypt.compare(password,this.password);
+  
+}
+const User=mongoose.model('User',userSchema);
+module.exports=User;
